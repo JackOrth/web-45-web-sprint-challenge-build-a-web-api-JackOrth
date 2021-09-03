941fef3d2dc2b386c9a98b881b2dfbbedff7aeb9
JackOrth <jackorth19@gmail.com>
2021-09-03 11:45:41 -0600

    Almost complete with MVP.

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,39 @@
 // add middlewares here related to projects
+const Project = require('./projects-model')
+
+async function validateProjectID(req,res,next) {
+    try{
+    const project = await Project.get(req.params.id)
+    if(project){
+        req.project = project
+        next()
+    }else {
+        next({
+            status: 404,
+            message: "No project found"
+        })
+    }
+    }catch(err){
+        next
+    }
+}
+async function validateProject(req,res,next) {
+    try{
+        const {name, description} = req.body
+        if (!name || !description){
+            res.status(400).json({
+                message: "Missing valid name or description"
+            })
+        }else{
+            next()
+        }
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = {
+    validateProjectID,
+    validateProject
+}
--- api/server.js
+++ api/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const server = express();
+const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
+
+
+server.use(express.json())
+server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
-console.log("Helloooo")
+server.use("*", (req,res)=>{
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+
 
 module.exports = server;
